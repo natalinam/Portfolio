6c151e34d79cbdf88a3be366feb5ec0998772aa3
NikitaMantia <nikitachilders@yahoo.com>
2023-02-01 11:22:33 -0600

    Integrated New and Create for Portfolio Actions

--- app/controllers/port_contents_controller.rb
+++ app/controllers/port_contents_controller.rb
@@ -2,4 +2,22 @@ class PortContentsController < ApplicationController
   def index
     @portfolio_items = PortContent.all
   end
-end
+
+  def new
+    @portfolio_items = PortContent.new
+  end
+
+  def create
+    @portfolio_items = PortContent.new(params.require(:port_content).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_items.save
+        format.html { redirect_to port_contents_path, notice: "Blog was successfully created." }
+        format.json { render :show, status: :created, location: @portfolio_items }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  end
